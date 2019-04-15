ea13be1fa12b53319f64ef176915d188ee2467f8
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-04-15 15:54:28 -0400

    Added homework file

---
+++ jsCh3.js
@@ -0,0 +1,24 @@
+function heeHaw(index)
+{
+    if(index >=1){
+        for(i=1; i <= index; i++)
+        {
+            if(i % 3 === 0 && i % 5 !== 0){
+                console.log("Hee!");
+            }
+            if(i % 3 !== 0 && i % 5 === 0){
+                console.log("Haw!");
+            }
+            if(i % 3 === 0 && i % 5 === 0){
+                console.log("Hee Haw!");
+            }
+            if(i % 3 !== 0 && i % 5 !== 0){
+                console.log(i);
+            }
+        }
+    }
+
+    else{
+        console.log("Number is less than 1, cannot process.");
+    }
+}
